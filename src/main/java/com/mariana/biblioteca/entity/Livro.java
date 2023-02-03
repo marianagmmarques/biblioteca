b11cef9885a21603689d321b63ffb8dbd8faf594
mariana <marigmmarques@gmail.com>
2023-02-03 18:03:29 -0300

    Adicionando entity

---
+++ src/main/java/com/mariana/biblioteca/entity/Livro.java
@@ -0,0 +1,89 @@
+package com.mariana.biblioteca.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Livro {
+    private int id;
+    private String titulo;
+    private String nome_autor;
+    private String texto;
+
+    private List<Categoria> categoria = new ArrayList<>();
+
+    public Livro(){
+        super();
+    }
+
+    public Livro(int id, String titulo, String nome_autor, String texto, List<Categoria> categoria) {
+        this.id = id;
+        this.titulo = titulo;
+        this.nome_autor = nome_autor;
+        this.texto = texto;
+        this.categoria = categoria;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getNome_autor() {
+        return nome_autor;
+    }
+
+    public void setNome_autor(String nome_autor) {
+        this.nome_autor = nome_autor;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public List<Categoria> getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(List<Categoria> categoria) {
+        this.categoria = categoria;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + id;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Livro other = (Livro) obj;
+        if (id != other.id)
+            return false;
+        return true;
+    }
+
+    
+}
