56a8f5281be7a134855e5d42d4a2a5ae790da6e2
mariana <marigmmarques@gmail.com>
2023-02-03 18:33:57 -0300

    Ajustando

--- src/main/java/com/mariana/biblioteca/entity/Categoria.java
+++ src/main/java/com/mariana/biblioteca/entity/Categoria.java
@@ -16,7 +16,7 @@ public class Categoria implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
     private String descricao;
